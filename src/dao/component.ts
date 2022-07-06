6b6133607653213b44407993076daf13138519e6
daw <962983053@qq.com>
2022-07-06 17:01:30 +0800

    feat 组件接口

---
+++ src/dao/component.ts
@@ -0,0 +1,28 @@
+import Component from '../models/component';
+import { processParams } from '../common'
+
+export default class ActivityDao {
+    async insert(data: object) {
+        const ripeData = await processParams(data, Component)
+        const activity = new Component(ripeData)
+        const res = await activity.save()
+        return res
+    }
+    async find() {
+        const res = await Component.find()
+        // 总条数
+        return res;
+    }
+    async update(id: number, data: Object) {
+        const res = await Component.updateOne({ id }, { ...data })
+        return res;
+    }
+    async findOne(id: number) {
+        let res = await Component.findOne({ id })
+        return res;
+    }
+    async delete(id: number) {
+        let res = await Component.deleteOne({ id })
+        return res
+    }
+}
